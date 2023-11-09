5978a4bc1a50bde36159eb4ff466316e25a37bb1
Giselle Maza <giselmaza27@gmail.com>
2023-11-09 14:12:51 -0300

    primer commit frontend

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/navbar'
+
+const App = () => {
+  return (
+    <div className='container'>
+      <div className='row'>
+        <Navbar></Navbar>
+      </div>
+    </div>
+  )
+}
+
+export default App
